8c4bbb9de62c78f3c4b0d06fab82f0e56f5c4084
huiylu <74116599+huiylu@users.noreply.github.com>
2021-01-11 11:41:13 -0800

    commit

--- controllers/leaveit.js
+++ controllers/leaveit.js
@@ -3,11 +3,11 @@ const ROUTER = EXPRESS.Router();
 
 //movies route
 ROUTER.get('/movies', (req, res) => {
-  res.render('leaveit/movies', {movies: ['forrest gump', 'Crash(not the Sebald / Cronenburg one)', 'Pretty much any comedy but especially anything starring Will Ferrel', 'Goodfellas', 'The Departed', 'Any Marvel Movie']});
+  res.render('leaveit/movie', {movies: ['forrest gump',  'The Room']});
 });
 //products route
 ROUTER.get('/products', (req, res) => {
-  res.render('leaveit/products', {products: ['single ply toilet paper', 'shitty paper towels', 'nonstick teflon cookwear', 'high socks', 'overhyped prestige television', 'in ear headphones', 'kerug coffee makers']});
+  res.render('leaveit/product', {products: ['single ply toilet paper', 'shitty paper towels']});
 });
 
 module.exports = ROUTER;
--- controllers/loveit.js
+++ controllers/loveit.js
@@ -3,11 +3,11 @@ const ROUTER = EXPRESS.Router();
 
 //animals route
 ROUTER.get('/animals', (req, res) => {
-  res.render('loveit/animals', {animals: ['sand crab', 'corny joke dog']});
+  res.render('loveit/animals', {animals: ['cats','turtles']});
 });
 //foods route
 ROUTER.get('/foods', (req, res) => {
-  res.render('loveit/foods', {foods: ['kimchee', 'salmon', 'crawdad boil', 'alomonds', 'peanut butter', 'feta cheese and olives', 'kale', 'brussel sprouts', 'sherbert', 'chili']});
+  res.render('loveit/foods', {foods: ['watermelon','burgers', 'Rice Noodle']});
 });
 
 module.exports = ROUTER;
