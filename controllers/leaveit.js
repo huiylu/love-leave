4518b2344e10139b3e4932289f0390c0ea134bfb
huiylu <74116599+huiylu@users.noreply.github.com>
2021-01-14 09:40:42 -0800

    Update leaveit.js

--- controllers/leaveit.js
+++ controllers/leaveit.js
@@ -3,11 +3,11 @@ const ROUTER = EXPRESS.Router();
 
 //movies route
 ROUTER.get('/movies', (req, res) => {
-  res.render('leaveit/movie', {movies: ['forrest gump',  'The Room']});
+  res.render('leaveit/movie', {movies: ['forrest gump',  'The Room','Any scary movie']});
 });
 //products route
 ROUTER.get('/products', (req, res) => {
-  res.render('leaveit/product', {products: ['single ply toilet paper', 'shitty paper towels']});
+  res.render('leaveit/product', {products: ['single ply toilet paper', 'shitty paper towels','cigars']});
 });
 
 module.exports = ROUTER;
